a9639bace50ce68799140b1860b8815ca8154e5c
prabhat-dsc <prabhat.dsc48@gmail.com>
2025-04-20 08:44:40 +0530

    data_ingestion file updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -23,13 +23,13 @@ class DataIngestion:
             logging.info("Getting raw data from source")
             data=pd.read_csv('raw.csv')
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
-            data.to_csv(self.ingestion_config.raw_data_path,index=True,header=True) 
+            data.to_csv(self.ingestion_config.raw_data_path,index=False,header=True) 
             logging.info(" Raw data ingestion completed")
             logging.info(" Data splition for train and test set started") 
             train_set,test_set= train_test_split(data,test_size=0.3,random_state=42)
 
-            train_set.to_csv(self.ingestion_config.train_data_path)
-            test_set.to_csv(self.ingestion_config.test_data_path)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
             logging.info(" Data Ingestion  completed")
             logging.info(" Returning train and test set path")
             return (
@@ -42,8 +42,7 @@ class DataIngestion:
             raise CustmeException (e,sys)
         
 
-"""
 if __name__=="__main__":
     obj=DataIngestion()
     obj.inititate_data_ingestion()
-"""
+
